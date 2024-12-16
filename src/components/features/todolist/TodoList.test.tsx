0f70bf010556df04655678132d5bb3fff6929fec
Sanshiroh SAKAI <sansyo@gmail.com>
2024-12-16 13:49:37 +0900

    Update: 新しい Todo の追加をテストと合わせて実装

--- src/components/features/todolist/TodoList.test.tsx
+++ src/components/features/todolist/TodoList.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoList } from "@/components/features/todolist/TodoList";
+import { userEvent } from "@testing-library/user-event";
 
 describe("TodoList", () => {
   it("初期状態では空のリストが表示される", () => {
@@ -12,5 +13,18 @@ describe("TodoList", () => {
     render(<TodoList />);
     expect(screen.getByRole("textbox")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "追加" })).toBeInTheDocument();
+  });
+
+  it("新しい Todo を追加できる", async () => {
+    render(<TodoList />);
+
+    const input = screen.getByRole("textbox");
+    const addButton = screen.getByRole("button", { name: "追加" });
+
+    await userEvent.type(input, "テストタスクを追加");
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("テストタスクを追加")).toBeInTheDocument();
+    expect(input).toHaveValue("");
   });
 });
--- src/components/features/todolist/TodoList.tsx
+++ src/components/features/todolist/TodoList.tsx
@@ -1,19 +1,57 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { v4 as uuid } from "uuid";
+import { useTodoStore } from "@/stores/todoStore";
+import { FormEvent, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export const TodoList = () => {
+  const [todoText, setTodoText] = useState("");
+  const todoList = useTodoStore((state) => state.todoList);
+  const addItem = useTodoStore((state) => state.actions.addItem);
+
+  const addTodoItem = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addItem({
+      id: uuid(),
+      title: todoText,
+      isCompleted: false,
+    });
+    setTodoText("");
+  };
+
   return (
     <>
       {/* 入力エリア */}
-      <form>
+      <form onSubmit={addTodoItem}>
         <div className="mb-4 flex items-center gap-2">
-          <Input type="text" placeholder="タスクを入力してください" />
-          <Button>追加</Button>
+          <Input
+            type="text"
+            placeholder="タスクを入力してください"
+            value={todoText}
+            onInput={(e) => setTodoText(e.currentTarget.value)}
+          />
+          <Button type="submit" disabled={!todoText}>
+            追加
+          </Button>
         </div>
       </form>
 
+      <p>{todoText}</p>
+
       {/* リスト */}
-      <ul className="space-y-2"></ul>
+      <ul className="space-y-2">
+        {todoList.map((todo) => (
+          <li
+            key={todo.id}
+            className={cn(
+              "flex justify-between items-center rounded-md border-border border py-2 px-3",
+            )}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
